1baaf6c9d545d976dd75266de94b6f1767661a67
rohit kumar <rohitwtbs@rohits-MacBook-Pro.local>
2022-12-02 00:15:30 +0530

    first commit of the app

---
+++ demo_app/Pods/Target Support Files/Pods-demo_app-demo_appUITests/Pods-demo_app-demo_appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demo_app_demo_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_demo_app_demo_appUITests
+@end
---
+++ demo_app/demo_app/demo_appApp.swift
@@ -0,0 +1,77 @@
+//
+//  demo_appApp.swift
+//  demo_app
+//
+//  Created by rohit kumar on 01/12/22.
+//
+
+import SwiftUI
+import TensorFlowLiteTaskVision
+
+@main
+struct demo_appApp: App {
+    @StateObject var dataModel = DataModel()
+    private var fetcher = Classification().identify()
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                GridView()
+            }
+            .environmentObject(dataModel)
+            .navigationViewStyle(.stack)
+        }
+    }
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//
+//        }
+//    }
+
+//    func classification() {
+//        guard let modelPath = Bundle.main.path(forResource: "birds_V1",
+//                                                    ofType: "tflite") else { return }
+//
+//        let options = ImageClassifierOptions(modelPath: modelPath)
+//
+//        // Configure any additional options:
+//        // options.classificationOptions.maxResults = 3
+//
+//        let classifier = try? ImageClassifier.classifier(options: options)
+//
+//        // Convert the input image to MLImage.
+//        // There are other sources for MLImage. For more details, please see:
+//        // https://developers.google.com/ml-kit/reference/ios/mlimage/api/reference/Classes/GMLImage
+//        guard let image = UIImage (named: "sparrow.jpg"), let mlImage = MLImage(image: image) else { return }
+//
+//        // Run inference
+//        let classificationResults = try? classifier?.classify(mlImage: mlImage)    }
+}
+
+
+class Classification {
+    func identify() {
+        guard let modelPath = Bundle.main.path(forResource: "birds_V1",
+                                                    ofType: "tflite") else { return }
+
+        let options = ImageClassifierOptions(modelPath: modelPath)
+
+        // Configure any additional options:
+        // options.classificationOptions.maxResults = 3
+
+        let classifier = try? ImageClassifier.classifier(options: options)
+
+        // Convert the input image to MLImage.
+        // There are other sources for MLImage. For more details, please see:
+        // https://developers.google.com/ml-kit/reference/ios/mlimage/api/reference/Classes/GMLImage
+        guard let image = UIImage (named: "sparrow.jpg"), let mlImage = MLImage(image: image) else { return }
+
+        // Run inference
+        let classificationResults = try? classifier?.classify(mlImage: mlImage)
+        print("classificationResults")
+    }
+        
+    
+}
+
+
